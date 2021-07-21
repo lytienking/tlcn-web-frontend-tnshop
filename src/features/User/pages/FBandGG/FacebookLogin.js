3a634aa7648ed59234c3bfc68d7bc22fcea8b16d
Ly Tien <lytienking@gmail.com>
2021-07-21 20:08:12 +0700

    fb login 3

--- src/features/User/pages/FBandGG/FacebookLogin.js
+++ src/features/User/pages/FBandGG/FacebookLogin.js
@@ -56,7 +56,7 @@ class FacebookLogin extends Component {
       ? (FacebookData = <div>Hi!</div>)
       : (FacebookData = (
           <FacebookLoginBtn
-            appId="383117903543380"
+            appId="151791427050643"
             autoLoad={false}
             fields="name,email,picture"
             callback={this.responseFacebook}
